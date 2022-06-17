942d27b5a725d342fbc2463e017308ccdf476254
Torrey Howell <torreyhowell97@gmail.com>
2022-06-17 17:40:40 -0400

    eslint setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import { Counter } from './features/counter/Counter'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Home from './pages/Home'
 
 function App() {
-  return <>Project</>
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test = 'test'
+
+console.log('test')
+
+function Home() {
+  return <div>Home</div>
+}
+
+export default Home
